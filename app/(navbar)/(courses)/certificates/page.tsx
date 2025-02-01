ff6e0e887401ead36e0416501255758f1755128c
VishaL6i9 <vishalkandakatla@gmail.com>
2025-02-01 12:01:51 +0530

    Added simple pages to render each module

---
+++ app/(navbar)/(courses)/certificates/page.tsx
@@ -0,0 +1,12 @@
+
+import Course_completion from "@/app/(navbar)/(courses)/certificates/course_completion";
+
+const HomePage = () => {
+    return (
+        <div>
+            <Course_completion />
+        </div>
+    );
+};
+
+export default HomePage;
---
+++ app/(navbar)/select-role/page.tsx
@@ -0,0 +1,9 @@
+// pages/page.tsx
+import React from 'react';
+import SelectRole from "@/app/(navbar)/select-role/SelectRole";
+
+const SelectRolePage: React.FC = () => {
+    return <SelectRole />;
+};
+
+export default SelectRolePage;
