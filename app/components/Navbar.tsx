da3336122afc06f97076ad1abdd6fdd2db9f0c7c
VishaL6i9 <vishalkandakatla@gmail.com>
2025-06-09 00:53:21 +0530

    Added Home Dashboard Button To NavBar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -104,9 +104,15 @@ const Navbar: React.FC = () => {
 
     const NavItems = () => (
         <>
-            <Button variant="ghost" asChild className="text-white hover:text-primary hover:bg-primary/10">
-                <Link href="/dashboard/home">Home</Link>
-            </Button>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" className="text-white hover:text-primary hover:bg-primary/10">Home</Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <DropdownMenuItem asChild><Link href="/dashboard/user-home">User</Link></DropdownMenuItem>
+                    <DropdownMenuItem asChild><Link href="/dashboard/instructor-home">Instructor</Link></DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
             
             <DropdownMenu>
                 <DropdownMenuTrigger asChild>
