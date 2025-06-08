27ac06ba8fec030cb98762a8ce775004d4d561d8
VishaL6i9 <vishalkandakatla@gmail.com>
2025-06-09 00:53:47 +0530

    Added Container For User Notification Dashboard Component

---
+++ app/dashboard/user-notification/page.tsx
@@ -0,0 +1,22 @@
+'use client';
+import React from 'react';
+import { NotificationProvider } from '@/app/components/user-notification-dashboard/contexts/NotificationContext';
+import Header from '@/app/components/user-notification-dashboard/Header';
+import Dashboard from '@/app/components/user-notification-dashboard/Dashboard';
+import ToastCenter from '@/app/components/user-notification-dashboard/ToastCenter';
+
+function App() {
+    return (
+        <NotificationProvider>
+            <div className="min-h-screen bg-gray-50">
+                <Header />
+                <main>
+                    <Dashboard />
+                </main>
+                <ToastCenter />
+            </div>
+        </NotificationProvider>
+    );
+}
+
+export default App;
