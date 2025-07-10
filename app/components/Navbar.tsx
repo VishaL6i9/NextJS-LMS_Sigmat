fb3130a1e46640a9e37065c8346d94fdd9617c4a
VishaL6i9 <vishalkandakatla@gmail.com>
2025-07-10 15:30:53 +0530

    feat: Add Sigmat logo to navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Link from 'next/link';
+import Image from 'next/image'; // Import the Image component
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import {
