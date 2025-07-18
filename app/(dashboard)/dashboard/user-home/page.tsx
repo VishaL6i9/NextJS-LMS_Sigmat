ab3b99067c4cabc9a8ec504332f04b0c39e533b9
VishaL6i9 <vishalkandakatla@gmail.com>
2025-07-18 18:37:36 +0530

    refactor: Migrated to shadcn/ui toast

--- app/(dashboard)/dashboard/user-home/page.tsx
+++ app/(dashboard)/dashboard/user-home/page.tsx
@@ -2,15 +2,16 @@
 
 import React from 'react';
 import { Dashboard } from '@/app/components/user-home-dashboard/Dashboard';
-import { ToastCenter } from '@/app/components/user-home-dashboard/ToastCenter';
 import MergedNavbar from '@/app/components/user-home-dashboard/MergedNavbar';
+import { NotificationProvider } from '@/app/components/user-home-dashboard/contexts/NotificationContext';
 
 export default function UserHome() {
     return (
         <div>
             <MergedNavbar />
-            <Dashboard />
-            <ToastCenter />
+            <NotificationProvider>
+                <Dashboard />
+            </NotificationProvider>
         </div>
     );
 }
