5d60c28ffed44c376caf2ca19a3d50eac2792aec
VishaL6i9 <vishalkandakatla@gmail.com>
2025-07-27 04:33:54 +0530

    feat: reworked on instructor-home page - Added Course Analytics section - Re-designed instructor dashboard

---
+++ app/(dashboard)/dashboard/instructor-home/layout.tsx
@@ -0,0 +1,12 @@
+'use client';
+const InstructorHomepageLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    return (
+        <div className="min-h-screen bg-gray-50">
+            <main className="flex-1 w-full">
+                {children}
+            </main>
+        </div>
+    );
+};
+
+export default InstructorHomepageLayout;
